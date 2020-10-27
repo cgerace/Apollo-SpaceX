b42c410c3cb7b8667b2bd90f140fc327f7711fb7
Christian Gerace <cgerace8@gmail.com>
2020-10-27 15:03:13 -0400

    Added datasources to server

--- start/server/src/index.js
+++ start/server/src/index.js
@@ -2,5 +2,21 @@ require('dotenv').config();
 
 const { ApolloServer } = require('apollo-server');
 const typeDefs = require('./schema');
+const { createStore } = require('./utils');
 
-const server = new ApolloServer({ typeDefs });
+const LaunchAPI = require('./datasources/launch');
+const UserAPI = require('./datasources/user');
+
+const store = createStore();
+
+const server = new ApolloServer({ 
+    typeDefs,
+    dataSources: () => ({
+        LaunchAPI: new LaunchAPI(),
+        UserAPI: new UserAPI({ store })
+    }) 
+});
+
+server.listen().then(({ url }) => {
+    console.log(`🚀 Server ready at ${url}`);
+  });
